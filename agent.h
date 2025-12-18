3bc2508aa599147193b560701821e4cb82e73c45
Mann Makwana <aiwithmann@gmail.com>
2025-12-18 09:33:11 +0530

    Add new member variables and update constructor

--- agent.h
+++ agent.h
@@ -5,6 +5,11 @@
 #include<vector>
 #include<cstdint>
 #include<stdexcept>
+#include<random>
+#include<initializer_list>
+#include<limits>
+#include<cmath>
+#include<algorithm>
 using FEATURE = std::array<uint8_t, 12>;
 using WEIGHT = std::array<float, 12 * 4>;
 using STATE = std::array<int,2>;
@@ -18,19 +23,28 @@ private:
     Snake snake;
     FEATURE feature;
     WEIGHT weights;
+    STATE currentState;
 
     float lr;
     float gamma;
     float eps;
+    float alpha;
     int steps;
     int maxIter;
+
+    std::mt19937 gen;
+    std::uniform_real_distribution<float> distEps{0.0f,1.0f};
+    std::uniform_int_distribution<int> distAction{0,4};
+    std::uniform_int_distribution<int> distRow{0,snake.getHeight()};
+    std::uniform_int_distribution<int> distCol{0, snake.getWidth()};
 
     FEATURE getFeature(const STATE& s) const;
     float getQ(FEATURE& s, ACTION a) const;
     int reward(const STATE& prevS) const;
+
 public:
-    Agent(const Snake& _snake, float learningRate, float gamma, float epsilon, int n, int maxIteration);
-    ACTION chooseAction(STATE s);
+    Agent(const Snake& _snake, float learningRate, float gamma, float epsilon, float alpha, int n, int maxIteration);
+    ACTION chooseAction(FEATURE& s);
     void train();
     void play();
 };
