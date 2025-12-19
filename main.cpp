56f7d690930a533a059ae3ef2b55eb109158ee1b
Mann Makwana <aiwithmann@gmail.com>
2025-12-19 14:29:55 +0530

    Increase training iterations for snake agent

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include "Snake.h"
 #include "agent.h"
+
 int main(){
     Snake game(10, 20);
-    Agent snakeagent(game, 0.5, 0.9, 0.2, 0.1,5,1000);
+    Agent snakeagent(game, 0.5, 0.9, 0.2, 0.1,5,50000);
     snakeagent.train();
     return 0;
 }
+
